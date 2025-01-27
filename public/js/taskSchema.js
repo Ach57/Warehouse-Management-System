24593d73faabd26ab38c5e659931926152872232
Achraf <155260097+Ach57@users.noreply.github.com>
2025-01-26 22:12:15 -0500

    Update taskSchema.js

--- public/js/taskSchema.js
+++ public/js/taskSchema.js
@@ -9,37 +9,87 @@ const taskSchema = new mongoose.Schema({
     metalFabrication: {
       startTime: { type: Date, default: null },
       endTime: { type: Date, default: null },
-      time: { type: Number, required: true }, // in hours
+      time: { type: Number, required: true }, //  Total time 
+      fullTime: {type: Number, default: 0}, // Total time of the task
+      totalTime: {type: String, default: null}, // difference between end time and start time
       status: { type: String, default: "pending" }, // pending, running, completed
-      employees: [{ type: String }], // List of employees assigned to this component
-      comment: { type: String, default: null },  // Optional comment
+      employees: {
+        type: [
+          {
+            name: { type: String },
+            startTime: { type: Date },
+            endTime: { type: Date, default: null },
+            contribution: { type: String, default: null },
+            comment: { type: String, default: null },
+          }
+        ],
+        default: [], // Ensures this field is always an array
+      },
     },
     paint: {
       time: { type: Number, required: true },
+      fullTime: {type: Number, default: 0}, // Total time of the task
       status: { type: String, default: "pending" },
-      employees: [{ type: String }], // List of employees assigned to this component
-      comment: { type: String, default: null },  // Optional comment
+      employees: {
+        type: [
+          {
+            name: { type: String },
+            startTime: { type: Date },
+            endTime: { type: Date, default: null },
+            contribution: { type: String, default: null },
+            comment: { type: String, default: null },
+          }
+        ],
+        default: [], // Ensures this field is always an array
+      },
+
     },
     assembly: {
       startTime: { type: Date, default: null },
       endTime: { type: Date, default: null },
       time: { type: Number, required: true }, // in hours
+      fullTime: {type: Number, default: 0}, // Total time of the task
+      totalTime: {type: String, default: null}, // difference between end time and start time
       status: { type: String, default: "pending" },
-      employees: [{ type: String }], // List of employees assigned to this component
-      comment: { type: String, default: null },  // Optional comment
+      employees: {
+        type: [
+          {
+            name: { type: String },
+            startTime: { type: Date },
+            endTime: { type: Date, default: null },
+            contribution: { type: String, default: null },
+            comment: { type: String, default: null },
+          }
+        ],
+        default: [], // Ensures this field is always an array
+      },
+
     },
     shipping: {
       startTime: { type: Date, default: null },
       endTime: { type: Date, default: null },
       time: { type: Number, required: true }, // in hours
+      fullTime: {type: Number, default: 0}, // Total time of the task
+      totalTime: {type: String, default: null}, // difference between end time and start time
       status: { type: String, default: "pending" },
-      employees: [{ type: String }], // List of employees assigned to this component
-      comment: { type: String, default: null },  // Optional comment
+      employees: {
+        type: [
+          {
+            name: { type: String },
+            startTime: { type: Date },
+            endTime: { type: Date, default: null },
+            contribution: { type: String, default: null },
+            comment: { type: String, default: null },
+          }
+        ],
+        default: [], // Ensures this field is always an array
+      },
+
     },
   },
   createdAt: {
     type: Date,
-    default: Date.now,
+    default : Date.now,
   },
 });
 
