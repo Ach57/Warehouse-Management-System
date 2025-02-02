5d65351c89c26357a36485db6b6e8812bc90dc38
Achraf <155260097+Ach57@users.noreply.github.com>
2025-02-01 20:45:36 -0500

    Update taskSchema.js

--- public/js/taskSchema.js
+++ public/js/taskSchema.js
@@ -5,6 +5,7 @@ const taskSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  priorityTask: { type: String, default: 'low'},
   components: {
     metalFabrication: {
       startTime: { type: Date, default: null },
